beb2013658065671cb33c8e89c73818e5d3160b4
Yury Chenrov <impact777@yande.ru>
2023-11-19 18:10:24 +0300

    fix: fixes after review

--- scripts/index.js
+++ scripts/index.js
@@ -7,26 +7,31 @@ const placesListContainer = document.querySelector(".places__list");
 // @todo: Функция создания карточки
 function createCard(link, name, callback) {
   const newCardElement = cardsTemplate.querySelector(".card").cloneNode(true);
+  const newCardElementImage = newCardElement.querySelector(".card__image");
 
-  newCardElement.querySelector(".card__image").src = link;
-  newCardElement.querySelector(".card__image").alt = name;
+  newCardElementImage.src = link;
+  newCardElementImage.alt = name;
   newCardElement.querySelector(".card__title").textContent = name;
   newCardElement
     .querySelector(".card__delete-button")
     .addEventListener("click", callback);
 
-  placesListContainer.append(newCardElement);
+  return newCardElement;
 }
+
 // @todo: Функция удаления карточки
 function deleteCard(event) {
   const element = event.target.closest(".card");
   element.remove();
 }
+
 // @todo: Вывести карточки на страницу
 function renderAllCards(cardsArray) {
- cardsArray.forEach(element => {
-  createCard(element.link, element.name, deleteCard)
- });
+  cardsArray.forEach((element) => {
+    placesListContainer.append(
+      createCard(element.link, element.name, deleteCard)
+    );
+  });
 }
 
 renderAllCards(window.initialCards);
